d3e92e04eaddf1f0e1540755c4f0898d544ecde0
subhamsoni <sonishubham65@gmail.com>
2020-07-25 15:16:35 +0530

    added readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# JUSPAY payment gateway for charging a customer without tokenization.
+> Author: [Shubham](http://sonishubham.com/)
+
+### RUN
+```
+keep the folder in localhost/juspay.
+```
--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 const BASE_URL = 'https://api.juspay.in';
 const MERCHANTID = "";
-const RETURN_URL = "http://localhost/axis/return.php";
+const RETURN_URL = "http://localhost/juspay/return.php";
 const APIKEY = "";
 const GetReadTimeout = 30;
 const ConnectTimeout = 15;
